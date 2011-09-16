90ad63198c9ff7bcb85f1a77888cf821d41caa9e
Rogelio Guzman <chinog9@gmail.com>
2011-09-15 19:44:48 -0500

    New app :)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TuFuturoUniversitario::Application.config.secret_token = '028866235bb803fbb49eef7c36c366cb2b6ad99dc19cbea9d67b75763f4ca38014951eb46219a1d33ffe616c0e246fac5633710d2dea07f18c350cf5d813692e'
