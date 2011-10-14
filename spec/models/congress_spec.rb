7f8b1fd8d4a236089be89dc0c1138b6e236dc0ec
Rogelio Guzman <chinog9@gmail.com>
2011-10-14 10:28:49 -0500

    Added some congress testing

--- spec/models/congress_spec.rb
+++ spec/models/congress_spec.rb
@@ -40,4 +40,47 @@ describe Congress do
     end
   end
 
+  context 'Instance Methods' do
+    describe 'set_address' do
+      context 'When there is no default address' do
+        it 'Assigns the text addres to the address' do
+          congress.address = 'An address'
+          congress.text_address = 'Some interesting address'
+          congress.set_address.should eql('Some interesting address')
+          congress.address.should eql('Some interesting address')
+        end
+      end
+      context 'When there is a default address' do
+        it 'Does not assigns the text addres to the address' do
+          congress.address = 'auditorio_luis_elizondo'
+          congress.set_address
+          congress.address.should eql('auditorio_luis_elizondo')
+        end
+      end
+    end
+  
+    describe 'text_address?' do
+      it 'Returns true if there is a text address' do
+        congress.text_address = 'Address'
+        congress.text_address?.should be_true
+      end
+      it 'Returns false if there is no text address' do
+        congress.text_address = ''
+        congress.text_address?.should be_false
+      end
+    end
+
+    describe 'default_address?' do
+      context 'Returns true with a default address' do
+        congress.address = 'auditorio_luis_elizondo'
+        congress.default_address?.should be_true
+      end
+
+      context 'Returns true with a default address' do
+        congress.address = 'an address'
+        congress.default_address?.should be_false
+      end
+    end
+
+  end
 end
