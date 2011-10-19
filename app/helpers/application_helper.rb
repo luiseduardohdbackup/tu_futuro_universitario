143c3b5dad27dbb729f2cf3088c0c02a3f9ae711
Ricardo <josericardo22@hotmail.com>
2011-10-19 10:27:51 -0500

    [Disen] - Push antes de que desmadre algo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def set_if_current(controlador,accion = "")
+      "activo" if controlador == params[:controller] and accion.present? ? accion == params[:action] : true
+  end
 end
