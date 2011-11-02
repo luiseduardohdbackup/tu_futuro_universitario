b55acf94d4ee7c8dec76d91195983b73fbee240a
Ricardo <josericardo22@hotmail.com>
2011-11-02 09:45:02 -0600

    Formato a formas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
   def set_if_current(controlador,accion = "")
       "activo" if controlador == params[:controller] and accion.present? ? accion == params[:action] : true
   end
+  def notice_trans(jQ)
+      jQ.to_s == "alert" ? "alert-message error" : "alert-message success"
+  end
 end
