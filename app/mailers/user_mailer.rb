682e096c33fe6a33e9b2de8026dbb95136899627
edo <elopez.deleon@gmail.com>
2011-11-21 11:58:47 -0600

    forma de contacto modificada con validaciones

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,21 +1,25 @@
 class UserMailer < ActionMailer::Base
-  default from: "viko.nava@gmail.com"
+  default from: "contacto@tufuturouniversitario.com"
 
   def new_application(application)
     @application = application
     @user = @application.user
-    @host = "http://localhost:3000"
     mail(:to => @application.congress.user.email, :subject => "Nueva Aplicacion al Congreso #{@application.congress.title}")    
   end
 
   def contact_form_send(info)
     @info = info
-    @owner = User.find_by_role("admin")
+    @owner = User.find_by_role("super_admin")
     mail(:to => @owner.email, :subject => "Mensaje de Contacto en TuFuturoUniversitario")
   end
 
   def scholarship_message(scholarship)
     @scholarship = scholarship
     mail(:to => @scholarship.email, :subject => "Aplicacion de una beca")
+  end
+
+  def scholarship_application(scholarship)
+    @scholarship = scholarship
+    mail(:to => User.find_by_role("admin"), :subject => "Aplicaron a una nueva beca en 'Tu Futuro Universitario'")
   end
 end
